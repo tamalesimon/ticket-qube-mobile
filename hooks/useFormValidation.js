12a4317580c7468b61b2970b7d5beb7a6b5b4573
tamalesimon <tamalesp@gmail.com>
2023-08-22 14:06:47 +0300

    refactored broken flow

--- hooks/useFormValidation.js
+++ hooks/useFormValidation.js
@@ -2,27 +2,28 @@ import { useState } from 'react';
 
 export const useFormValidation = () => {
     const [formData, setFormData] = useState({
-    firstName:'',
-    lastName:'',
-    email:'',
-    password:'',
-    phoneNumber: '',
-    confirmPassword:'',
-    dateOfBirth: '1987-01-10T00:00:00.00Z',
-    userRole: "CLIENT" });
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        phoneNumber: '',
+        confirmPassword: '',
+        dateOfBirth: '1987-01-10T00:00:00.00Z',
+        userRole: "CLIENT"
+    });
     const [formErrors, setFormErrors] = useState({
-    emailError: '',
-    passwordError: '',
-    firstNameError:'',
-    lastNameError:'',
-    phoneNumberError:'',
-    dateOfBirthError:'',
-    userRoleError:'',
-    confirmPasswordError:''
+        emailError: '',
+        passwordError: '',
+        firstNameError: '',
+        lastNameError: '',
+        phoneNumberError: '',
+        dateOfBirthError: '',
+        userRoleError: '',
+        confirmPasswordError: ''
     });
 
     const validateForm = () => {
-        const {email, password, firstName, lastName, confirmPassword } = formData;
+        const { email, password, firstName, lastName, confirmPassword } = formData;
         let isValid = true;
         const errors = {};
 
@@ -33,18 +34,6 @@ export const useFormValidation = () => {
         if (password.trim() === '') {
             isValid = false;
             errors.passwordError = 'Password is required';
-        }
-        if (firstName.trim() === '') {
-            isValid = false;
-            errors.passwordError = 'Password is required';
-        }
-        if (lastName.trim() === '') {
-            isValid = false;
-            errors.passwordError = 'Password is required';
-        }
-        if (password !== confirmPassword) {
-            isValid = false;
-            errors.passwordError = 'Password need to match';
         }
         setFormErrors(errors)
         return isValid;
