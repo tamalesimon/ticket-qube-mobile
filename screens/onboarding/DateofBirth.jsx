c9cf703c3b3a059fe500fbe566b29e38d2c6b291
tamalesimon <tamalesp@gmail.com>
2023-11-09 20:47:58 +0300

    add date of birth

--- screens/onboarding/DateofBirth.jsx
+++ screens/onboarding/DateofBirth.jsx
@@ -7,22 +7,26 @@ import GenericButton from '../../components/buttons/genericButton';
 import globalStyles from '../../styles/globalStyles';
 import PopularLocations from './PopularLocations';
 import { Footer } from '../event/components';
+import { TextInput } from 'react-native-gesture-handler';
 
 export default function DateofBirth({ navigator }) {
     return (
         <SafeAreaView style={{ backgroundColor: COLORS.white, flex: 1, padding: 23 }}>
             <View style={{ flexDirection: 'column', gap: 8, marginTop: 32 }}>
-                <View style={{ flexDirection: 'column',  }}>
+                <View style={{ flexDirection: 'column', }}>
                     <Text style={{ ...globalStyles.Heading4, textAlign: 'left', }} >Enter your date of Birth</Text>
-                    <Text style={styles.subComment}>Please make sure you're above the age of 18 years.</Text>
+                    <Text style={styles.subComment}>Please tell us how old you are.</Text>
                 </View>
-                <View style={{ marginTop: 24, gap: 12 }}>
-                    <InputField
-                        placeholder="DD/MM/YYYY"
-                        placeholderTextColor={COLORS.gray400}
-                        inputType={'DOB'}
-                    />
-
+                <View style={{ marginTop: 24, gap: 12, flexDirection: 'row' }}>
+                    <View style={styles.textInputContainer}>
+                        <TextInput style={styles.textInput} placeholder='DD' />
+                    </View>
+                    <View style={styles.textInputContainer}>
+                        <TextInput style={styles.textInput} placeholder='MM' />
+                    </View>
+                    <View style={styles.textInputContainer}>
+                        <TextInput style={styles.textInput} placeholder='YYYY' />
+                    </View>
                     {/* <Text style={{ marginHorizontal: 24, textAlign: "center", color: COLORS.gray400, fontSize: 14, fontFamily: FONTS.NotoSansJPRegular }}>Or</Text>
                     <TouchableOpacity>
                         <View style={{ flexDirection: 'row', gap: 12, borderRadius: 16, borderStyle: 'dashed', borderWidth: 1, padding: 24, borderColor: COLORS.gray200, width: '100%', paddingHorizontal: 69.5, alignItems: 'center' }}>
@@ -52,5 +56,26 @@ const styles = StyleSheet.create({
         fontWeight: 500,
         fontSize: 14,
         color: COLORS.primaryBase
-    }
+    },
+    textInputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: COLORS.gray50,
+        borderRadius: 16,
+        padding: 20,
+        width: '21%'
+    },
+    textInput: {
+        // marginLeft: 12,
+        // height: 40,
+        flex: 1,
+        textAlign: 'center',
+        width: '100%',
+        borderRadius: 16,
+        borderWidth: 0, // This will remove the border
+        borderColor: 'transparent', // This will make the border color transparent
+        borderRadius: 0, // This will make the border edges straight
+        backgroundColor: 'transparent', // This will make the background color transparent
+    },
 })
